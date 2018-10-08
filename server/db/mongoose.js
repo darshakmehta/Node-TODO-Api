ed5f0a85bc08d2cb5b4fd2e3e7de364fdd12aba3
tech-boy <darshak_mehta@live.com>
2018-10-07 22:40:01 -0400

    Test the DELETE /todos:/id route

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://admin:admin27@ds125453.mlab.com:25453/todo-api" || 'mongodb://localhost:27017/TodoApp' );
+mongoose.connect('mongodb://localhost:27017/TodoApp' );//"mongodb://admin:admin27@ds125453.mlab.com:25453/todo-api"
 mongoose.set('useCreateIndex', true);
 mongoose.set('useFindAndModify', false);
 
